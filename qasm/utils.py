8b93525c12d04b24b04b8a75d9efd2d7682c0b6e
Scott Pakin <pakin@lanl.gov>
2016-06-28 09:10:26 -0600

    Minor modifications

--- qasm/utils.py
+++ qasm/utils.py
@@ -8,15 +8,16 @@ from qasm.globals import *
 # Define a function that maps from a symbol to a number, creating a
 # new association if necessary.
 def symbol_to_number(sym):
+    global sym2num, next_sym_num
     try:
         return sym2num[sym]
     except KeyError:
-        global next_sym_num
         next_sym_num += 1
         sym2num[sym] = next_sym_num
         return next_sym_num
 
 # Define a function to abort the program on an error.
 def abend(str):
+    global progname
     sys.stderr.write("%s: %s\n" % (progname, str))
     sys.exit(1)
