d48954b90d71ce0a388b4561d4466c76a95d5111
Scott Pakin <pakin@lanl.gov>
2016-07-08 14:44:23 -0600

    Introduce a 10% fudge factor for chain strength

--- qasm/update.py
+++ qasm/update.py
@@ -21,14 +21,16 @@ def assign_chain_strength(ch_str, qubo):
     """Define a strength for each user-specified and automatically generated
     chain, and assign strengths to those chains."""
     qasm.chain_strength = ch_str
+    fudge_factor = 1.1
     if qasm.chain_strength == None:
-        # Chain strength defaults to the maximum strength in the data.
+        # Chain strength defaults to the maximum strength in the data times a
+        # fudge factor.
         try:
-            qasm.chain_strength = -max([abs(w) for w in qasm.strengths.values()])
+            qasm.chain_strength = -max([abs(w) for w in qasm.strengths.values()]) * fudge_factor
         except ValueError:
             # No strengths -- use weights instead.
             try:
-                qasm.chain_strength = -max([abs(w) for w in qasm.weights.values()])
+                qasm.chain_strength = -max([abs(w) for w in qasm.weights.values()]) * fudge_factor
             except ValueError:
                 # No weights or strengths -- arbitrarily choose -1.
                 qasm.chain_strength = -1.0
