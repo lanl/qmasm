b61619a6ccbe9ef53f9720dba230706e4fb69c18
Scott Pakin <pakin@lanl.gov>
2019-10-09 14:28:24 -0600

    Move existing code into an old/ subdirectory
    My plan is to start from scratch and do it right.

---
+++ qmasm/top.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+###################################
+# Quantum Macro Assembler         #
+# By Scott Pakin <pakin@lanl.gov> #
+###################################
+
+class QMASM:
+    pass
