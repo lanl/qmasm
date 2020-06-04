21109e46382da828d8453c916b2127ec476daea9
Scott Pakin <pakin@lanl.gov>
2020-06-03 18:27:06 -0600

    Update the Python package metadata

--- setup.py
+++ setup.py
@@ -7,24 +7,29 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "qmasm",
-      version = "4.0",
-      description = "Quantum Macro Assembler",
-      author = "Scott Pakin",
-      author_email = "pakin@lanl.gov",
-      classifiers = ["Topic :: Software Development :: Compilers"],
-      url = "https://github.com/lanl/qmasm",
-      license = "BSD",
-      keywords = "quantum assembler d-wave",
+setup(name = 'qmasm',
+      version = '4.0',
+      description = 'Quantum Macro Assembler',
+      author = 'Scott Pakin',
+      author_email = 'pakin@lanl.gov',
+      classifiers = [
+          'Topic :: Software Development :: Compilers',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Programming Language :: Python :: 3',
+          'Intended Audience :: Developers'],
+      url = 'https://github.com/lanl/qmasm',
+      license = 'BSD-ish',
+      keywords = ['quantum', 'annealing', 'macro', 'assembler', 'd-wave'],
       entry_points = {
-          "console_scripts": ["qmasm = qmasm.__main__:main"]
+          'console_scripts': ['qmasm = qmasm.__main__:main']
       },
-      scripts = ["scripts/qb2qmasm",
-                 "extras/qmasm-gen-all-to-all",
-                 "extras/qmasm-gen-chimera",
-                 "extras/qmasm-gen-current"],
-      packages = find_packages("src"),
-      package_dir = {"": "src"},
-      python_requires = ">= 3.8",
-      install_requires = ["dwave-ocean-sdk"]
+      scripts = ['scripts/qb2qmasm',
+                 'extras/qmasm-gen-all-to-all',
+                 'extras/qmasm-gen-chimera',
+                 'extras/qmasm-gen-current'],
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      python_requires = '>= 3.8',
+      install_requires = ['dwave-ocean-sdk']
 )
